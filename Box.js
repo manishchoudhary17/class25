84330521dcac8256e23b1ad235bb561fac3ade4b
manishchoudhary17 <82105441+manishchoudhary17@users.noreply.github.com>
2021-05-05 20:46:06 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,18 @@
+class Box extends BaseClass {
+  constructor(x, y, width, height) {
+   super(x,y,width,height);
+   this.image = loadImage("sprites/wood1.png");
+
+
+  }
+
+    
+};
+
+//inheritance
+//parent class - baseclass
+//child class - subclass
+//child class inherits properties and functions from parent class
+//BaseClass
+//pig, box, log, bird - chilc classes of BaseClass
+
